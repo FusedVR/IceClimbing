f571bdc7f639bdca9740ceca48c7333e9557f280
Vasanth Mohan <vmohan7@stanford.edu>
2017-06-24 14:06:21 -0700

    Added Haptics

--- Assets/Pull.cs
+++ Assets/Pull.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 using Valve.VR.InteractionSystem;
 
 public class Pull : MonoBehaviour {
+
+    public Interactable interactable;
+
     [Tooltip("The pivot point of our climbing")]
     public GameObject TrackedObject;
 
@@ -16,10 +19,17 @@ public class Pull : MonoBehaviour {
     [HideInInspector]
     public bool cangrip;
 
+    private Hand hand;
+
 	// Use this for initialization
 	void Start () {
+        interactable.onAttachedToHand += Interactable_onAttachedToHand;
         prevpos = TrackedObject.transform.position;
 	}
+
+    private void Interactable_onAttachedToHand(Hand hand) {
+        this.hand = hand;
+    }
 
     public Vector3 Delta() {
         return prevpos - TrackedObject.transform.position;
@@ -28,10 +38,15 @@ public class Pull : MonoBehaviour {
     private void LateUpdate() {
         // keep track of previous positions in order to calculate deltas
         prevpos = TrackedObject.transform.position;
+
+        if (cangrip) {
+            hand.controller.TriggerHapticPulse(); //uses the hand we saved and adds haptic feedback to it when we climb
+        }
     }
 
     void OnTriggerEnter(Collider other) {
         Vector3 velocity = Delta() / Time.fixedDeltaTime;
+
 
         if (velocity.magnitude > 5f && other.tag == "Climbable") { //only climb climbable objects
             snow.Emit(20);
